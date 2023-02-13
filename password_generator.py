f041a1227d05946ec4a2beaa0ccaa373d787843f
KHaidome <obichi7@gmail.com>
2023-02-13 17:06:09 +0100

    Update

--- password_generator.py
+++ password_generator.py
@@ -1,9 +1,9 @@
 """Create a random password generator using loops"""
 
-student_heights = [180, 124, 165, 173, 189, 169, 146]
-max_number = 0
+import random
 
-for i in student_heights:
-    if i > max_number:
-        max_number = i
-print(f"The highest score in the class is: {max_number}")
+word = "hello"
+
+for i in range(0,5):
+    random_word = random.choice(word)
+print(random_word)
